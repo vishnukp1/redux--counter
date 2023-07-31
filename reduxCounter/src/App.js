6a52ae0dd9ee16b46209e50504bcb1e8bc00ace0
wishtony <vishnutoanother@gmail.com>
2023-07-31 19:40:00 +0530

    second commit

--- reduxCounter/src/App.js
+++ reduxCounter/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { decreament,increament } from './folder/couterSlice';
+import { decreament,increament } from './component/couterSlice';
 function App() {
   const state1 = useSelector((state)=>state.counter)
   const dispacth= useDispatch()
--- reduxCounter/src/index.js
+++ reduxCounter/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {store} from "./folder/store"
+import {store} from "./component/store"
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
